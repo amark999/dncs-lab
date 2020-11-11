2eb18ca9e952598da95ecdf6213b07fc76c810ef
Amar <amar.kasibovic@studenti.unitn.it>
2020-11-11 17:26:18 +0100

    Removed some useless files, added .sh files for all VMs

--- switch.sh
+++ switch.sh
@@ -4,3 +4,4 @@ apt-get install -y tcpdump
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
 
 # Startup commands for switch go here
+
